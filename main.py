f405bcbd230b4feb56f9314ca3410ce61bfcea4e
Azamat Musaev <onlyforusersgames@gmail.com>
2021-11-16 01:34:32 +0500

    add func to check if the board is valid

--- main.py
+++ main.py
@@ -10,7 +10,30 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+# Check if the board is valid
+def valid(bo, num, pos):
+    # Checking rows by looking at every column in a row
+    for i in range(len(bo[0])):
+        if bo[pos[0][i] == num and pos[1]] != i:
+            return False
 
+    # Check columns by looking at every row in a column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+# Print Board func.
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
@@ -25,9 +48,10 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-
+# Find empty cell func.
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 print(i, j)
+
