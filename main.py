61ee9311a2cba8b84cf991f0972273c842c04251
Azamat Musaev <onlyforusersgames@gmail.com>
2021-11-16 01:10:32 +0500

    add find empty cell func

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ board = [
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("-----------------------")
+            print("-------------------")
 
-        for j in range(len(bo)):
+        for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
                 print("|", end="")
 
@@ -25,4 +25,9 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                print(i, j)
